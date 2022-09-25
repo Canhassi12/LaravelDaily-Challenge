462a309688e151011490d850f677a25e025a1c5f
Canhassi <arthurcanhassi@gmail.com>
2022-09-25 10:56:39 -0300

    feat: button for return to dashboard

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Company</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    <a href="http://127.0.0.1:8000/">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline">
+            Dashboard
+        </button>    
+    </a>
     <div class="flex justify-center mt-20">
         @isset($messages)
             <h3 class="text-red-500">{{ $messages }}</h3>
--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Employee</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    <a href="http://127.0.0.1:8000/">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline">
+            Dashboard
+        </button>    
+    </a>
     <div class="flex justify-center mt-20">
         @isset($messages)
             <h3 class="text-red-500">{{ $messages }}</h3>
@@ -20,7 +25,7 @@
         @isset($error)
             <h3 class="text-red-500">{{ $error }}</h3>
         @endisset
-
+        
         <x-employee-form :companies="$companies"/>
 
         <div>
@@ -55,7 +60,8 @@
                     @endforeach 
                 </table>
                 {{ $employees->links() }}                   
-            @endisset         
+            @endisset  
+                   
         </div>
     </div>
 </body>
